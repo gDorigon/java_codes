04e482c2dba72e4a47386e46943e89a82bad541c
Guilherme <guidbovo@gmail.com>
2022-07-08 14:27:04 -0300

    código básico de divisão

---
+++ Divisao_basica/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int a, b;
+        double resultado;
+
+        a = 5;
+        b = 4;
+
+        resultado =(double) a / b;
+
+        System.out.println("A divisão é igual a: "+ resultado);
+    }
+}
